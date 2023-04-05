9f8ad0ca5aebba9de618824cca12194e72e53fc8
Sumit Tale <sumittale2001@gmail.com>
2023-04-05 16:32:24 +0530

    loginform and register added

---
+++ src/Components/Desktop10.js
@@ -0,0 +1,196 @@
+import React from "react";
+
+const Desktop10 = () => {
+  return (
+    <>
+      <div id="outer" className=" mx-60 my-10">
+        <div id="filter" className="p-5 ">
+          <span className="rounded-3xl p-2  bg-[#EAF0FF] shadow-lg m-2">
+            hig to low
+          </span>
+          <span className="rounded-3xl p-2  bg-[#EDEDED] shadow-lg m-2">
+            hig to low
+          </span>
+          <span className="rounded-3xl p-2  bg-[#EDEDED] shadow-lg m-2">
+            hig to low
+          </span>
+          <span className="rounded-3xl p-2  bg-[#EDEDED] shadow-lg m-2">
+            hig to low
+          </span>
+        </div>
+        <div className="bg-white flex  flex-row w-82 p-2 bg-red-100 ">
+          <div id="result " className="w-3/4">
+            <h1 className="text-red-600 text-xl ">Result</h1>
+
+            <div className="p-2 bg-blue-100 h-42 w-auto rounded-lg m-2 flex ">
+              <div className="flex  flex-row w-82">
+                <div className="bg-[#F8AF6A] w-52 h-34 rounded-lg"></div>
+                <div className="p-1 text-xs flex flex-col text-slate-600">
+                  <span>December 21,2021</span>
+                  <div className="flex flex-col p-1">
+                    <h1 className="text-lg font-bold text-blue-900">
+                      What is Design Thinking ?
+                    </h1>
+                    <span>Tutor Name</span>
+                    <div class="flex flex-row">
+                      <span className="bg-[#05FF00] w-2 h-2 rounded-lg"></span>
+                      <span className="">online </span>
+                    </div>
+                    <div className="flex  gap-1 items-center xs:gap-1 xs:text-xs rounded-3xl p-1 bg-[#FFFFFF]  m-1 w-24 font-bold text-orange-400">
+                      {" "}
+                      <i class="fa-solid fa-indian-rupee-sign"></i>
+                      <label className="text-lg">
+                        59<span className="text-xs font-thin">(per hour)</span>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 bg-blue-100 h-42 w-auto rounded-lg m-2 flex  ">
+              <div className="flex  flex-row w-82">
+                <img
+                  className="rounded-full h-32 w -32 mx-10 border-red-600 border-2"
+                  src="https://www.fakepersongenerator.com/Face/female/female20161025115339539.jpg"
+                  alt=""
+                />
+                <div className="p-1 text-xs flex flex-col text-slate-600 py-8">
+                  <div className="flex flex-col p-1">
+                    <h1 className="text-lg font-bold text-blue-900">
+                      User name
+                    </h1>
+                    <span>Tutor Name</span>
+                    <span className="">online</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 bg-blue-100 h-42 w-auto rounded-lg m-2 flex  ">
+              <div className="flex  flex-row w-82 ">
+                <img
+                  className="rounded-full h-32 w -32 mx-10 border-red-600 border-2"
+                  src="https://www.fakepersongenerator.com/Face/female/female20161025115339539.jpg"
+                  alt=""
+                />
+                <div className="p-1 text-xs flex flex-col text-slate-600 py-8">
+                  <div className="flex flex-col p-1">
+                    <h1 className="text-lg font-bold text-blue-900">
+                      User name
+                    </h1>
+                    <span>Tutor Name</span>
+                    <span className="">online</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 bg-blue-100 h-42 w-auto rounded-lg m-2 flex ">
+              <div className="flex  flex-row w-82">
+                <div className="bg-[#F8AF6A] w-52 h-34 rounded-lg"></div>
+                <div className="p-1 text-xs flex flex-col text-slate-600">
+                  <span>December 21,2021</span>
+                  <div className="flex flex-col p-1">
+                    <h1 className="text-lg font-bold text-blue-900">
+                      What is Design Thinking ?
+                    </h1>
+                    <span>Tutor Name</span>
+                    <div className="flex flex-row">
+                    <span className="bg-[#05FF00] w-2 h-2 rounded-lg"></span>
+                    <span className="">online</span>
+                    </div>
+                    <div className="flex  gap-1 items-center xs:gap-1 xs:text-xs rounded-3xl p-1 bg-[#FFFFFF]  m-1 w-24 font-bold text-orange-400">
+                      {" "}
+                      <i class="fa-solid fa-indian-rupee-sign"></i>
+                      <label className="text-lg">
+                        59<span className="text-xs font-thin">(per hour)</span>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 bg-blue-100 h-42 w-auto rounded-lg m-2 flex ">
+              <div className="flex  flex-row w-82">
+                <div className="bg-[#F8AF6A] w-52 h-34 rounded-lg"></div>
+                <div className="p-1 text-xs flex flex-col text-slate-600">
+                  <span>December 21,2021</span>
+                  <div className="flex flex-col p-1">
+                    <h1 className="text-lg font-bold text-blue-900">
+                      What is Design Thinking ?
+                    </h1>
+                    <span>Tutor Name</span>
+                    <span className="bg-[#05FF00] w-2 h-2 rounded-lg"></span>
+                    <span className="">online</span>
+                    <div className="flex  gap-1 items-center xs:gap-1 xs:text-xs rounded-3xl p-1 bg-[#FFFFFF]  m-1 w-24 font-bold text-orange-400">
+                      {" "}
+                      <i class="fa-solid fa-indian-rupee-sign"></i>
+                      <label className="text-lg">
+                        59<span className="text-xs font-thin">(per hour)</span>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="recomendation " className=" w-1/4 mx-4">
+            <h1 className="text-red-600 text-xl">Recomendation</h1>
+
+            <div className="p-1  h-16 w-72 rounded-sm m-2 flex  ">
+              <div className="flex  flex-row w-82">
+                <img
+                  className="rounded-full h-12 w-12 border-red-600 border-2"
+                  src="https://www.fakepersongenerator.com/Face/female/female20161025115339539.jpg"
+                  alt=""
+                />
+                <div className="px-2 text-xs flex flex-row text-slate-600">
+                  <div className="flex flex-col">
+                    <h1 className="text-sm font-bold text-blue-900">
+                      User name
+                    </h1>
+                    <span>Tutor Name</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p-1  h-16 w-72 rounded-sm m-2 flex  ">
+              <div className="flex  flex-row w-82">
+                <img
+                  className="rounded-full h-12 w-12 border-red-600 border-2"
+                  src="https://www.fakepersongenerator.com/Face/female/female20161025115339539.jpg"
+                  alt=""
+                />
+                <div className="px-2 text-xs flex flex-row text-slate-600">
+                  <div className="flex flex-col">
+                    <h1 className="text-sm font-bold text-blue-900">
+                      User name
+                    </h1>
+                    <span>Tutor Name</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p-1  h-16 w-72 rounded-sm m-2 flex  ">
+              <div className="flex  flex-row w-82">
+                <img
+                  className="rounded-full h-12 w-12 border-red-600 border-2"
+                  src="https://www.fakepersongenerator.com/Face/female/female20161025115339539.jpg"
+                  alt=""
+                />
+                <div className="px-2 text-xs flex flex-row text-slate-600">
+                  <div className="flex flex-col">
+                    <h1 className="text-sm font-bold text-blue-900">
+                      User name
+                    </h1>
+                    <span>Tutor Name</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Desktop10;
