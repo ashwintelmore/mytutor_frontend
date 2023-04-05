a1d37bb8ab3dc11a2146aab9058e4e8bbdd6ac30
ashwin <telmoreashwin2001@gmail.com>
2023-04-05 16:00:40 +0530

    about and all post done

--- src/Components/VerticalNav.js
+++ src/Components/VerticalNav.js
@@ -7,11 +7,11 @@ const VerticalNav = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleSidebar = () => setIsOpen(!isOpen);
-
+  console.log("tes")
   return (
     <div className=" footer w-16 bg-indigo-100 text-lg flex flex-col dark:bg-zinc-900 dark:text-yellow-300 xs:bg-orange-100 xs:h-14 xs:p-1     ">
-     <div className="flex flex-col items-center p-8 gap-10   xs:flex xs:p-2 xs:gap-2 xs:flex-row  xs:items-center  xs:justify-evenly  ">
-     <Link to={"/"}>  <i className=" bg-slate-400 cursor-pointer dark:bg-slate-700 p-2 rounded-full fa-solid fa-house house"></i></Link>
+      <div className="flex flex-col items-center p-8 gap-10   xs:flex xs:p-2 xs:gap-2 xs:flex-row  xs:items-center  xs:justify-evenly  ">
+        <Link to={"/"}>  <i className=" bg-slate-400 cursor-pointer dark:bg-slate-700 p-2 rounded-full fa-solid fa-house house"></i></Link>
         <i className=" fa-solid fa-comment"></i>
         <div>
           <i
