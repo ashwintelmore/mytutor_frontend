e27632927a31ab79bf38263c03a1b143cda0f2f7
pranav date <pranavdate908@gmail.com>
2023-04-12 11:28:32 +0530

    new profile page modification and made responsive

--- src/Pages/Profile/Education.js
+++ src/Pages/Profile/Education.js
@@ -39,76 +39,81 @@ function Education({ show, setShow }) {
   if (!show)
     return null;
   return (
-    <div className="flex w-full items-center justify-center xs:flex-col absolute top-4 left-0">
-      <div className=" bg-[#fff] w-4/6 h-auto p-1 rounded-3xl flex flex-col items-center justify-center  shadow-md shadow-black xs:flex-col xs:w-11/12">
-        <h2 className="text-[#f48c2b] text-left">Education</h2>
+    <div className="flex w-full  items-center justify-center xs:flex-col absolute top-4 left-0">
+      <div className=" bg-[#fff] relative w-4/6 h-auto p-3 rounded-3xl flex flex-col  items-center justify-center  shadow-md shadow-slate-600 xs:flex-col xs:w-11/12">
+        <h2 className="text-[#f48c2b] top-0 left-2 text-lg p-2 absolute ">Education</h2>
 
-        <div className="flex p-2 gap-1  w-full justify-between text-sm xs:text-xs xs:gap-0 xs:p-1 xs:flex-col xs:w-full">
-          <div className="flex flex-col  w-2/5 p-4 justify-around xs:w-full">
-            <label className="w-1/5 text-lg xs:text-base">Name of Education</label>
-            <input className="rounded-xl w-full shadow-sm shadow-black" type="text"
+        <div className="flex p-1   w-full justify-between text-sm xs:text-xs xs:gap-0 xs:p-1 xs:flex-col xs:w-full">
+          <div className="flex flex-col relative  w-[45%] p-2  xs:w-full">
+            <label className="w-full p-2 text-base xs:text-base">Name of Education</label>
+            <input className="rounded-xl w-full shadow-sm shadow-black p-2" type="text"
               name="title"
               onChange={(e) => setUserData({ ...userData, [e.target.name]: e.target.value })}
               value={userData.title}
             />
+            <label className="text-xs ml-2 p-1">Write something</label>
           </div>
-          <div className="flex flex-col  w-1/2   p-4  xs:w-full ">
-            <label className="w-1/2 text-lg xs:text-sm xs:w-3/5 ">Schoole/College/University name</label>
-            <input className="rounded-xl w-9/12 xs:w-4/5 xs:p-2 shadow-sm shadow-black" type="text"
-              name="orginization"
+          <div className="flex flex-col   w-[45%] p-2 xs:w-full">
+            <label className="w-full p-2 text-base xs:text-base">School/College/University</label>
+            <input className="rounded-xl w-full shadow-sm shadow-black p-2" type="text"
+              name="title"
               onChange={(e) => setUserData({ ...userData, [e.target.name]: e.target.value })}
-              value={userData.orginization}
+              value={userData.title}
             />
+            <label className="text-xs ml-2 p-1">Write something</label>
           </div>
         </div>
 
-        <div className="flex p-2 gap-1  w-full justify-between text-sm xs:text-xs xs:gap-0 xs:p-1 xs:flex-col xs:w-full">
-          <div className="flex flex-col   w-2/5  p-4 justify-around xs:w-full  ">
-            <label className="w-1/5 text-lg ">From</label>
-            <input className="  rounded-xl w-full  shadow-sm shadow-black" type="text"
+        <div className="flex p-1   w-full justify-between text-sm xs:text-xs xs:gap-0 xs:p-1 xs:flex-col xs:w-full">
+          <div className="flex flex-col   w-[45%]  p-2  xs:w-full  ">
+            <label className="w-full text-base p-2 ">From</label>
+            <input className="  rounded-xl w-full  p-2 shadow-sm shadow-black" type="date"
               name="from"
               onChange={(e) => setUserData({ ...userData, [e.target.name]: e.target.value })}
               value={userData.from}
             />
+              <label className="text-xs ml-2 p-1">Write something</label>
           </div>
-          <div className="flex  flex-col  w-1/2  p-4 xs:w-full">
-            <label className="w-1/2 text-lg xs:w-1/4">Location</label>
-            <input className="  rounded-xl w-9/12   shadow-sm shadow-black" type="text"
+          <div className="flex  flex-col  w-[45%]  p-2 xs:w-full">
+            <label className="w-full p-2 text-base xs:w-1/4">Location</label>
+            <input className="  rounded-xl w-full p-2  shadow-sm shadow-black" type="text"
               name="location"
               onChange={(e) => setUserData({ ...userData, [e.target.name]: e.target.value })}
               value={userData.location}
 
             />
+              <label className="text-xs ml-2 p-1">Write something</label>
           </div>
         </div>
 
-        <div className="flex p-2 gap-1  w-full justify-between text-sm xs:text-xs xs:gap-0 xs:p-1 xs:flex-col xs:w-full">
-          <div className="flex flex-col  xs:w-full  w-2/5 p-4 justify-around  ">
-            <label className="w-1/5 text-lg ">To</label>
-            <input className="  rounded-xl w-full  shadow-sm shadow-black" type="text"
+        <div className="flex p-1    w-full justify-between text-sm xs:text-xs xs:gap-0 xs:p-1 xs:flex-col xs:w-full">
+          <div className="flex flex-col  xs:w-full  w-[45%] p-2 justify-around  ">
+            <label className="w-full text-base p-2">To</label>
+            <input className="  rounded-xl w-full p-2  shadow-sm shadow-black" type="date"
               name="to"
               onChange={(e) => setUserData({ ...userData, [e.target.name]: e.target.value })}
               value={userData.date}
             />
+              <label className="text-xs ml-2 p-1">Write something</label>
           </div>
         </div>
 
-        <div className="p-2">
+        <div className="p-1">
           <h2>Description :</h2>
-          <textarea rows={5} cols={50} className="rounded-2xl xs:h-24 xs:w-11/12 shadow-sm shadow-black"
+          <textarea rows={5} cols={100} className="rounded-2xl pl-2 xs:h-24 w-4/5 xs:w-11/12 shadow-sm shadow-black"
             name="descrp"
             onChange={(e) => setUserData({ ...userData, [e.target.name]: e.target.value })}
             value={userData.descrp}
           ></textarea>
         </div>
 
-        <div className="flex p-2 xs:w-full xs:justify-evenly gap-2 w-2/5 justify-between">
-          <button className="xs:w-2/5 bg-[#F8AF6A] rounded-xl p-2 w-2/5"
+        <div className="flex p-2 xs:w-full xs:justify-evenly gap-2 w-full justify-end">
+          <button className="xs:w-2/5 bg-[#f68f30] text-white rounded-xl p-2 w-[15%]"
 
             onClick={() => onAddDetails()}
           >Add
           </button>
-          <button className=" xs:w-2/5 bg-[#F8AF6A] rounded-xl p-2 w-2/5"
+          <button className=" xs:w-2/5 bg-[#f68f30] text-white rounded-xl p-2 w-[15%]"
             onClick={() => setShow(!show)}
           >Cancel
           </button>
