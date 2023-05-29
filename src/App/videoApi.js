5ccd4ed4408a4fcd35f7da4a4490839904c57f0c
ashwin <telmoreashwin2001@gmail.com>
2023-05-29 11:45:15 +0530

    video calling

--- src/App/videoApi.js
+++ src/App/videoApi.js
@@ -5,18 +5,20 @@ const VIDEOSDK_TOKEN = process.env.REACT_APP_VIDEOSDK_TOKEN
 export const generateMeetingId = async (data) => {
     const token = VIDEOSDK_TOKEN
     try {
-    const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
-        method: "POST",
-        headers: {
-            authorization: `${token}`,
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({}),
-    });
-    //Destructuring the roomId from the response
+        const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
+            method: "POST",
+            headers: {
+                authorization: `${token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({}),
+        });
+
+        console.log('res', res)
+        //Destructuring the roomId from the response
 
 
-   
+
         const { roomId } = await res.json();
         return roomId
     } catch (error) {
