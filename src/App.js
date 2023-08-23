032b9a93dfcd35f6d7c80d5fe673ed2f3c870690
ashwin <telmoreashwin2001@gmail.com>
2023-08-24 01:35:29 +0530

    little more optimise

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
+import React, { useState, lazy, Suspense  } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 // import Backrop from "./Components/Backrop";
 import Dummy from "./Components/Helper/Dummy";
+
 import Home from "./Pages/Home/Home";
 import Header from "./Header/Header";
-import Profile from "./Pages/Profile/Profile";
-import ShowProfile from "./Pages/Profile/ShowProfile";
 import VerticalNav from "./Header/VerticalNav";
 import LoginForm from "./Auth/LoginForm";
 import Register from "./Auth/Register";
-import { useAuth } from "./providers/auth";
-import { ProfileProvider } from "./providers/profile";
-import Comment from "./Pages/Posts/comments/Comment";
-import Post from "./Pages/Posts/Post";
-import SearchResult from "./Pages/Search/SearchResult";
-import Appointement from "./Pages/Request/Appointement";
 import Sidebar from "./Header/Sidebar";
-import SidebarClose from "./Header/SidebarClose";
-import React, { useState, useEffect } from "react";
-import Favourite from "./Pages/Favourite/Favourite";
-import AboutUs from "./Header/AboutUs";
-import Calender from "./Components/Calender";
-import Notification from "./Pages/Notification/Notification";
 import PrivateRoute from "./PrivateRoute";
 import VerifyEmail from "./Auth/VerifyEmail";
 import Landing_page from "./Pages/Home/Landing_page";
+import Loader from './Components/Helper/Loader';
+
+import { useAuth } from "./providers/auth";
+
+
+
+const Profile = lazy(()=>import("./Pages/Profile/Profile"))
+const ShowProfile = lazy(()=>import("./Pages/Profile/ShowProfile"))
+const Post = lazy(()=>import("./Pages/Posts/Post"))
+const SearchResult = lazy(()=>import("./Pages/Search/SearchResult"))
+const Appointement = lazy(()=>import("./Pages/Request/Appointement"))
+const SidebarClose = lazy(()=>import("./Header/SidebarClose"))
+const Favourite = lazy(()=>import("./Pages/Favourite/Favourite"))
+const Calender = lazy(()=>import("./Components/Calender"))
+const Notification = lazy(()=>import("./Pages/Notification/Notification"))
+
+
+
+
+const AboutUs = lazy(() => import("./Header/AboutUs"))
 
 const App = () => {
   const auth = useAuth();
@@ -35,81 +44,84 @@ const App = () => {
   };
   return (
     <>
-      <BrowserRouter>
-        <Header setOpen={setOpen} open={open} />
-        <SidebarClose setOpen={setOpen} open={open} />
-        <Sidebar open={open} />
-        <div className="flex w-full mt-16  dark:bg-color-16 bg-color-3 relative">
-          <VerticalNav />
-          <Routes>
-            <Route path="/" element={<Landing_page />} />
-            <Route path="/explore" element={<Home />} />
-            <Route
-              path="login"
-              element={
-                auth.user._id ? (
-                  <Navigate replace to="/explore" />
-                ) : (
-                  <LoginForm />
-                )
-              }
-            />
-            <Route
-              path="register"
-              element={
-                auth.user._id ? (
-                  <Navigate replace to="/profile" />
-                ) : (
-                  <Register />
-                )
-              }
-            />
-            {/* <Route path="/register" element={<Register />} /> */}
-            {/* <Route path="/login" element={<LoginForm />} /> */}
+      <ErrorBoundary fallback={<div>Something went wrong please contact support on ashwintelmore@gmail.com</div>}>
+        <Suspense fallback={<Loader />} >
+          <BrowserRouter>
+            <Header setOpen={setOpen} open={open} />
+            <SidebarClose setOpen={setOpen} open={open} />
+            <Sidebar open={open} />
+            <div className="flex w-full mt-16  dark:bg-color-16 bg-color-3 relative">
+              <VerticalNav />
+              <Routes>
+                <Route path="/" element={<Landing_page />} />
+                <Route path="/explore" element={<Home />} />
+                <Route
+                  path="login"
+                  element={
+                    auth.user._id ? (
+                      <Navigate replace to="/explore" />
+                    ) : (
+                      <LoginForm />
+                    )
+                  }
+                />
+                <Route
+                  path="register"
+                  element={
+                    auth.user._id ? (
+                      <Navigate replace to="/profile" />
+                    ) : (
+                      <Register />
+                    )
+                  }
+                />
+                {/* <Route path="/register" element={<Register />} /> */}
+                {/* <Route path="/login" element={<LoginForm />} /> */}
 
-            <Route
-              path="/:email/:token"
-              element={
-                auth.user.isVerified ? (
-                  <Navigate replace to="/profile" />
-                ) : (
-                  <VerifyEmail />
-                )
-              }
-            />
-            <Route
-              path="/VerifyEmail"
-              element={
-                !auth.user._id ?
-                  <Navigate replace to="/explore" />
-                  :
-                  auth.user.isVerified ? (
-                    <Navigate replace to="/explore" />
-                  ) : (
-                    <VerifyEmail />
-                  )
-              }
-            />
+                <Route
+                  path="/:email/:token"
+                  element={
+                    auth.user.isVerified ? (
+                      <Navigate replace to="/profile" />
+                    ) : (
+                      <VerifyEmail />
+                    )
+                  }
+                />
+                <Route
+                  path="/VerifyEmail"
+                  element={
+                    !auth.user._id ?
+                      <Navigate replace to="/explore" />
+                      :
+                      auth.user.isVerified ? (
+                        <Navigate replace to="/explore" />
+                      ) : (
+                        <VerifyEmail />
+                      )
+                  }
+                />
 
-            <Route element={<PrivateRoute />}>
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/showProfile/:id?" element={<ShowProfile />} />
-              <Route path="/dummy" element={<Dummy />} />
-              <Route path="/comment" element={<Comment />} />
-              <Route path="/appointement" element={<Appointement />} />
-              <Route path="/favourite" element={<Favourite />} />
-              <Route path="/notification" element={<Notification />} />
-            </Route>
-            <Route path="/search/:catName?" element={<SearchResult />} />
-            <Route path="/calender" element={<Calender />} />
-            <Route path="/postcontent/:id?" element={<Post />} />
-            <Route path="/aboutus" element={<AboutUs />} />
-            
+                <Route element={<PrivateRoute />}>
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/showProfile/:id?" element={<ShowProfile />} />
+                  <Route path="/dummy" element={<Dummy />} />
+                  <Route path="/appointement" element={<Appointement />} />
+                  <Route path="/favourite" element={<Favourite />} />
+                  <Route path="/notification" element={<Notification />} />
+                </Route>
+                <Route path="/search/:catName?" element={<SearchResult />} />
+                <Route path="/calender" element={<Calender />} />
+                <Route path="/postcontent/:id?" element={<Post />} />
+                <Route path="/aboutus" element={<AboutUs />} />
 
-            <Route path="*" element={<Dummy />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+
+                <Route path="*" element={<Dummy />} />
+              </Routes>
+            </div>
+          </BrowserRouter>
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 };
