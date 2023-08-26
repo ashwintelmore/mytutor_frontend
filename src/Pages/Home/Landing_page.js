302bb0bce5a36d9e928799d352dc8638418072e7
pranav date <pranavdate908@gmail.com>
2023-08-26 19:34:26 +0530

    animation

--- src/Pages/Home/Landing_page.js
+++ src/Pages/Home/Landing_page.js
@@ -10,6 +10,7 @@ import landingimage8 from "../../assets/YT Thumbnail2.png";
 import landingimage9 from "../../assets/YT Thumbnail3.png";
 import landingimage10 from "../../assets/landing10.png";
 import { Link } from "react-router-dom";
+import { delay, motion, useScroll } from "framer-motion";
 
 function Landing_page() {
   return (
@@ -19,7 +20,9 @@ function Landing_page() {
           {" "}
           Your Knowledge Sharing{" "}
         </h1>
-        <h1 className="text-5xl font-extrabold mb-2 text-[#0D0E2F] sm:text-2xl ">Platform</h1>
+        <h1 className="text-5xl font-extrabold mb-2 text-[#0D0E2F] sm:text-2xl ">
+          Platform
+        </h1>
         <p className="text-[#898CA9] text-[15px]  sm:text-[11px] sm:mt-3 ">
           Are you passionate about sharing your expertise and knowledge with
           others?{" "}
@@ -30,29 +33,53 @@ function Landing_page() {
         <p className="text-[#898CA9] text-[15px]  sm:text-[11px]">
           Look no further – MyTutor is here.
         </p>
-        <Link to="/explore" className="w-28 mt-7 text-center border text-color-3 border-color-7 h-auto p-2 rounded-lg bg-gradient-to-r from-[#933FFE] to-[#18C8FF]">
+        <Link
+          to="/explore"
+          className="w-28 mt-7 text-center border text-color-3 border-color-7 h-auto p-2 rounded-lg bg-gradient-to-r from-[#933FFE] to-[#18C8FF]">
           Explore
         </Link>
       </div>
-      <div className="flex justify-around items-center   mt-[17%] sm:hidden  pt-5 w-full">
+
+      <motion.div
+        className="flex justify-around items-center   mt-[17%] sm:hidden  pt-5 w-full"
+        animate={{ x: [50, 150, 50], opacity: 1, scale: 1 }}
+        transition={{
+          duration: 5,
+          delay: 0.3,
+          ease: [0.5, 0.71, 1, 1.5],
+        }}
+        initial={{ opacity: 0, scale: 0.5 }}
+        whileHover={{ scale: 1.2 }}>
         <div>
           <img src={landingimage1} className="h-60 w-72"></img>
         </div>
         <div>
           <img src={landingimage2} className="h-60 w-72"></img>
         </div>
-      </div>
+      </motion.div>
 
       <div className="flex flex-col w-[45%]  mt-[11%]    items-center sm:w-full">
-        <h1 className="text-5xl font-extrabold mt-2 text-[#0D0E2F] sm:text-2xl">
-          {" "}
-          How It Works:{" "}
-        </h1>
+        <motion.h1 whileHover={{ scale: 1.2 }}>
+          <h1 className="text-5xl font-extrabold mt-2 text-[#0D0E2F] sm:text-2xl">
+            {" "}
+            How It Works:{" "}
+          </h1>
+        </motion.h1>
       </div>
+
       <div className="flex mt-[2%] sm:w-full  relative">
-        <div className="w-[90%] bg-color-13 h-fit sm:hidden ">
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ x: 0 }}
+          transition={{
+            duration: "2",
+            delay: "1",
+            ease: [0.5, 0.71, 1, 1.5],
+          }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          className="w-[90%] bg-color-13 h-fit sm:hidden ">
           <img className="h-[600px]  w-full " src={landingimage10}></img>
-        </div>
+        </motion.div>
         <div className="absolute w-[75%] flex flex-col  -right-56 sm:w-full sm:left-0 sm:ml-10  top-10">
           <label className="mt-5 text-2xl text-[#0D0E2F] font-semibold sm:text-xl sm:font-extrabold">
             Step 1. Sign Up
@@ -94,6 +121,7 @@ function Landing_page() {
           </p>
         </div>
       </div>
+
       <div className="flex flex-col w-[45%]  mt-[11%] sm:w-full  sm:mt-[80%]    items-center   sm:items-center">
         <h1 className="text-5xl font-extrabold mt-2 text-[#0D0E2F] sm:text-2xl">
           {" "}
@@ -110,11 +138,33 @@ function Landing_page() {
           Look no further – MyTutor is here.
         </p>
       </div>
+
       <div className="w-[80%] flex  mt-[7%] items-center justify-around sm:w-full  sm:flex-col-reverse ">
         <div className="w-1/2 sm:w-[90%]">
-          <img src={landingimage3}></img>
+          <motion.img
+          initial={{ opacity: 0, scale: 0    }}
+          // animate={{ x: -500 }}
+          transition={{
+            duration: "1"
+           
+            // delay: "1",
+            // ease: [0.5, 0.71, 1, 1.5],
+          }}
+          whileInView={{ opacity: 1, scale: 1, x: 0 }}
+            src={landingimage3}></motion.img>
         </div>
-        <div className="w-1/2 flex flex-col gap-3 sm:w-full sm:items-center">
+
+        <motion.div
+          initial={{ opacity: 0, scale: 0   }}
+          // animate={{ y: -500 }}
+          transition={{
+            duration: "1"
+          
+            // delay: "1",
+            // ease: [0.5, 0.71, 1, 1.5],
+          }}
+          whileInView={{ opacity: 1, scale: 1, y: 0 }}
+          className="w-1/2 flex flex-col gap-3 sm:w-full sm:items-center">
           <h1 className="text-4xl font-extrabold mt-2 text-[#0D0E2F] sm:text-2xl">
             Empower Others
           </h1>
@@ -123,11 +173,19 @@ function Landing_page() {
             be empowering individuals to expand their horizons and achieve their
             goals.
           </p>
-        </div>
+        </motion.div>
       </div>
 
       <div className="w-[80%] flex mt-[7%] items-center justify-around ml-10 sm:w-full sm:flex-col">
-        <div className="w-1/2 flex flex-col gap-3 sm:w-full sm:items-center ">
+        <motion.div
+          initial={{ y: 150 }}
+          animate={{ y: 0 }}
+          transition={{
+            duration: "2",
+            delay: "1",
+            ease: [0.5, 0.71, 1, 1.5],
+          }}
+          className="w-1/2 flex flex-col gap-3 sm:w-full sm:items-center ">
           <h1 className="text-4xl font-extrabold mt-2  text-[#0D0E2F] sm:text-2xl">
             Earn While Sharing
           </h1>
@@ -135,17 +193,43 @@ function Landing_page() {
             Turn your expertise into earnings. Receive compensation for the
             knowledge you share with learners.
           </p>
-        </div>
+        </motion.div>
         <div className="w-1/2 sm:w-[90%]">
-          <img className="ml-10 sm:ml-0" src={landingimage4}></img>
+          <motion.img
+            initial={{ x: 150 }}
+            animate={{ x: 0 }}
+            transition={{
+              duration: "2",
+              delay: "1",
+              ease: [0.5, 0.71, 1, 1.5],
+            }}
+            className="ml-10 sm:ml-0"
+            src={landingimage4}></motion.img>
         </div>
       </div>
 
       <div className="w-[80%] flex mt-[7%] items-center justify-around sm:w-full  sm:flex-col-reverse ">
         <div className="w-1/2 sm:w-[90%]">
-          <img className="h-60 ml-10 w-80" src={landingimage5}></img>
+          <motion.img
+            initial={{ x: -170 }}
+            animate={{ x: 0 }}
+            transition={{
+              duration: "2",
+              delay: "1",
+              ease: [0.5, 0.71, 1, 1.5],
+            }}
+            className="h-60 ml-10 w-80"
+            src={landingimage5}></motion.img>
         </div>
-        <div className="w-1/2 flex flex-col gap-3 sm:w-full sm:items-center">
+        <motion.div
+          initial={{ y: -150 }}
+          animate={{ y: 0 }}
+          transition={{
+            duration: "2",
+            delay: "1",
+            ease: [0.5, 0.71, 1, 1.5],
+          }}
+          className="w-1/2 flex flex-col gap-3 sm:w-full sm:items-center">
           <h1 className="text-4xl font-extrabold mt-2 text-[#0D0E2F] sm:text-2xl">
             Seamless Video Calls
           </h1>
@@ -153,22 +237,41 @@ function Landing_page() {
             Our platform provides a smooth and secure video calling experience.
             Connect with your students from anywhere in the world.
           </p>
-        </div>
+        </motion.div>
       </div>
       <div className="w-[80%] flex mt-[7%] items-center justify-around ml-10 sm:w-full sm:flex-col">
-        <div className="w-1/2 flex flex-col gap-3 sm:w-full sm:items-center">
+        <motion.div
+          initial={{ y: 150 }}
+          animate={{ y: 0 }}
+          transition={{
+            duration: "2",
+            delay: "1",
+            ease: [0.5, 0.71, 1, 1.5],
+          }}
+          className="w-1/2 flex flex-col gap-3 sm:w-full sm:items-center">
           <h1 className="text-4xl font-extrabold mt-2 text-[#0D0E2F] sm:text-2xl">
-          Flexible Schedule
+            Flexible Schedule
           </h1>
           <p className="sm:text-[11px] sm:w-[80%]">
-          Set your availability according to your convenience. MyTutor lets you manage your time and commitments effortlessly.
+            Set your availability according to your convenience. MyTutor lets
+            you manage your time and commitments effortlessly.
           </p>
-        </div>
+        </motion.div>
         <div className="w-1/2 sm:w-[85%]">
-          <img className="ml-10 sm:ml-0" src={landingimage6}></img>
+          <motion.img
+            initial={{ x: 150 }}
+            animate={{ x: 0 }}
+            transition={{
+              duration: "2",
+              delay: "1",
+              ease: [0.5, 0.71, 1, 1.5],
+            }}
+            className="ml-10 sm:ml-0"
+            src={landingimage6}></motion.img>
         </div>
       </div>
-      <div className="flex flex-col w-[45%]  mt-[11%]    items-center sm:w-full sm:items-center sm:justify-center sm:mt-[15%]">
+
+      <div className="flex flex-col w-[50%]  mt-[11%]    items-center sm:w-full sm:items-center sm:justify-center sm:mt-[15%]">
         <h1 className="text-5xl font-extrabold mt-2 text-[#0D0E2F] sm:text-2xl">
           {" "}
           Key Features of MyTutor{" "}
@@ -178,6 +281,7 @@ function Landing_page() {
           Your Earnings."
         </p>{" "}
       </div>
+
       <div className="flex gap-10 justify-center w-full mt-7 sm:flex-col sm:justify-center sm:items-center">
         <div>
           <img src={landingimage7} className="h-60 w-72"></img>
@@ -189,7 +293,6 @@ function Landing_page() {
           <img src={landingimage9} className="h-60 w-72"></img>
         </div>
       </div>
-    
 
       <div className="flex flex-col w-[55%] mt-[8%]  mb-20  items-center sm:w-full sm:mt-[500px] ">
         <h1 className="text-5xl font-extrabold mt-2 text-[#0D0E2F] sm:text-2xl">
